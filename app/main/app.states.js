19ac5dfa4998f480ddc3532478c629061dc059bc
yujinpan <yujinpan@cmstop.com>
2018-03-23 15:17:26 +0800

    修改login登陆跳转

--- app/main/app.states.js
+++ app/main/app.states.js
@@ -21,8 +21,38 @@ export const loginState = {
     parent: 'app',
     name: 'login',
     url: '/login',
-    component: 'login'
+    component: 'login',
+    resolve: {
+        returnTo: returnTo
+    }
 };
+
+/**
+ * “登录”状态的解析函数，用于在成功登录后确定要返回的状态。
+ *
+ * 如果用户最初重定向到登录状态（由于requiresAuth重定向），则返回toState / params
+ * 他们被重定向。 否则，如果他们直接转换，则返回fromState / params。 除此以外
+ * 返回主“家”状态。
+ */
+returnTo.$inject = ['$transition$'];
+function returnTo($transition$) {
+    // 用户被重定向到登录状态（例如，在尝试激活联系人时通过requiresAuth挂钩）
+    // 返回到原始的尝试目标状态（例如，联系人）
+    if ($transition$.redirectedFrom() != null) {
+        return $transition$.redirectedFrom().targetState();
+    }
+
+    let $state = $transition$.router.stateService;
+
+    // 用户未被重定向到登录状态; 他们以某种方式直接激活登录状态。
+    // 将它们返回到它们来自的状态。
+    if ($transition$.from().name !== '') {
+        return $state.target($transition$.from(), $transition$.params("from"));
+    }
+
+    // 如果fromState的名字是空的，那么这是最初的转换。 只要将它们返回到本地状态
+    return $state.target('home');
+}
 
 export const messagesState = {
     parent: 'app',
--- app/main/login.component.js
+++ app/main/login.component.js
@@ -2,29 +2,44 @@
  * loginComponent的controller
  */
 class LoginController {
-    constructor($state) {
-        this.name = 'Richard';
+    constructor($state, LoadingIndicatorService, AuthService) {
+        this.$state = $state;
+        this.LoadingIndicatorService = LoadingIndicatorService;
+        this.AuthService = AuthService;
+    }
+    submit() {
+        const returnToOriginalState = () => {
+            let state = this.returnTo.state();
+            let params = this.returnTo.params();
+            let options = Object.assign({}, this.returnTo.options(), { reload: true });
+            $state.go(state, params, options);
+        };
+        this.AuthService.authenticate(this.username, this.password)
+            .then(returnToOriginalState);
     }
 }
-LoginController.$inject = ['$state'];
+LoginController.$inject = ['$state', 'LoadingIndicatorService', 'AuthService'];
 
 // login component
 export const login = {
     controller: LoginController,
-    template:`
-        <h1>Login {{$ctrl.name}}.</h1>
+    bindings: {
+        returnTo: '<'
+    },
+    template: `
+        <h1>Login myself@angular.dev,devgal@angular.dev,devguy@angular.dev</h1>
         <div class="input-group">
             <label class="col-sm-3">user(myself@angular.dev)</label>
             <div class="col-sm-9">
-                <input>
+                <input placeholder="your username" ng-model="$ctrl.username">
             </div>
         </div>
         <div class="input-group">
             <label class="col-sm-3">password</label>
             <div class="col-sm-9">
-                <input>
+                <input placeholder="password" ng-model="$ctrl.password">
             </div>
         </div>
-        <button type="button" class="btn btn-primary">submit</button>
+        <button type="button" class="btn btn-primary" ng-click="$ctrl.submit()">submit</button>
     `
 };
